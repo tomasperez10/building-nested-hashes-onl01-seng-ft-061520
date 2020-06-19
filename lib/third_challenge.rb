6ffe328dddb10b0343e0a683c409e8c0e45841ef
Tomas Perez <tomasitopg6@gmail.com>
2020-06-19 23:51:55 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,7 +27,8 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        name: "Lady Capulet"
+        name: "Lady Capulet",
+        age: "51"
         },
       :heroine => {
         #your key/value pairs here
